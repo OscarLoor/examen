6c8781281b83f972fcb1b6d129fffe15ca531ede
Oscar <oscar@imac-de-patricia.local>
2017-06-15 22:54:51 -0500

    Initial Commit

---
+++ examen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  examen
+//
+//  Created by Oscar on 15/6/17.
+//  Copyright © 2017 Oscar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
